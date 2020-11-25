83051e67006f815146eebc417670e191811d54c0
Gwen Beebe <gbeebe@chipindy.org>
2020-11-25 14:33:09 -0800

    Testing git connection

--- QD Snippets.sql
+++ QD Snippets.sql
@@ -1,6 +1,6 @@
-------------------------------------------------
------------ calculate client age ---------------
-------------------------------------------------
+---------------------------------------------------
+------------- calculate client age ----------------
+---------------------------------------------------
 
 (
 	CONVERT(
@@ -18,3 +18,28 @@
 		)
 )
 /10000
+
+
+---------------------------------------------------
+---- get most recent VI-SPDAT score for client ----
+---------------------------------------------------
+
+(
+	SELECT max(ScoreTotal) 
+	FROM VISPDAT V (NOLOCK) 
+	INNER JOIN
+		(SELECT VI.ClientID, 
+			(SELECT TOP 1 VIA.VulnerabilityID 
+				FROM VulnerabilityIndex VIA (NOLOCK) 
+				INNER JOIN Assessment A (NOLOCK) 
+				ON A.AssessmentID = VIA.AssessmentID
+					AND VIA.ActiveStatus<>'D' 
+					AND A.ActiveStatus<>'D' 
+					And VIA.ClientID=cmClient.ClientID
+				ORDER BY VIA.AssessmentDate DESC, VulnerabilityID ) 
+			AS VID
+		FROM VulnerabilityIndex VI (NOLOCK) 
+		GROUP BY VI.ClientID	
+		) LastVI 
+		ON LastVI.VID = V.VulnerabilityID		 
+)
