999201da296bc5ed05ac4ff6e48aa698ef11f486
Gwen Beebe <gbeebe@chipindy.org>
2020-12-22 16:00:55 -0800

    first day of diversion request logic

--- Data Requests/00_load_files.R
+++ Data Requests/00_load_files.R
@@ -9,10 +9,11 @@ ReportStart <- ymd("20191001")
 ReportEnd <- ymd("20201001")
 
 Demographics <- 
-  read_excel(paste0(directory, "/Demographics - 12.1.17 - 12.17.20.xlsx")) %>%
+  read_excel(paste0(directory, "/Demographics - 12.1.17 - 12.17.20 v2.xlsx")) %>%
   mutate(RaceDesc = if_else(RaceDesc %in% c("Data not collected", "Client doesn't know", "Client refused") |
-                                 is.na(RaceDesc), "Unknown", RaceDesc))
-
+                                 is.na(RaceDesc), "Unknown", RaceDesc),
+         HUDEthnicity = if_else(HUDEthnicity %in% c("Data not collected", "Client doesn't know", "Client refused") |
+                                             is.na(HUDEthnicity), "Unknown", HUDEthnicity))
 Programs <- 
   read_excel(paste0(directory, "/Programs 12.16.20.xlsx")) 
 
@@ -26,38 +27,3 @@ all_data <- Enrollments %>%
 rm(list = ls()[!(ls() %in% c("all_data"))])
 
 save.image("images/all_data.RData")
-
-
-active_in_period <- all_data %>%
-  filter(EnrollDate < ReportEnd &
-           (is.na(ExitDate) | ExitDate >= ReportStart))
-
-diversion_households_active_in_period <- active_in_period %>%
-  filter((ProgramName == "Diversion" 
-          | ProgramName == "IHN - Diversion"
-          | ProgramName == "YHDP - Diversion")
-         & `Head of Household?` == "Yes")  %>%
-  group_by(ClientID) %>%
-  arrange(EnrollDate) %>%
-  slice(1L) %>%
-  ungroup()
-
-diversion_households_enhanced <- diversion_households_active_in_period %>%
-  left_join(all_data %>%
-              filter(ProgramType == "Emergency shelter") %>%
-              select("ClientID", "EnrollDate"),
-            by = "ClientID") %>%
-  group_by(ClientID) %>%
-  mutate(entered_shelter = if_else(EnrollDate.x <= EnrollDate.y, 1, 0),
-         entered_shelter = if_else(is.na(max(entered_shelter)), 0, max(entered_shelter)),
-         entered_shelter = if_else(entered_shelter == 1, "Yes", "No")) %>%
-  select(-EnrollDate.y) %>%
-  arrange(EnrollDate.x) %>%
-  slice(1L) %>%
-  ungroup()
-
-# x <- table(diversion_households_enhanced$RaceDesc, diversion_households_enhanced$entered_shelter)
-# prop.table(x)
-# addmargins(x)
-# summary(x)
-# mosaicplot(x)
