51b2d5ad306bdfd2225174135fa59a5a24c53b03
Gwen Beebe <gbeebe@chipindy.org>
2021-01-15 08:35:43 -0800

    added most recent EnrollID logic to QD

--- QD Snippets.sql
+++ QD Snippets.sql
@@ -89,8 +89,8 @@ GROUP BY E.[ClientID]
 ---------------------------------------------------
 
 (SELECT max(case 
-		when E.[EnrollDate] is null then getdate() 
-			else E.[EnrollDate] end
+		when E.[ExitDate] is null then getdate() 
+			else E.[ExitDate] end
 		) 
 FROM Enrollment E (NOLOCK)  
 INNER JOIN EnrollmentCase EC(NOLOCK)  	
@@ -99,12 +99,52 @@ ON E.CaseID=EC.CaseID
 WHERE E.ClientID = cmClient.[ClientID] 
 GROUP BY E.[ClientID])
 
+---------------------------------------------------
+--------------- calculate COVID score -------------
+---------------------------------------------------
 
+(SELECT min(COVIDScore)
+FROM
+	(SELECT PL.[CaseID] AS [CaseID],
+		(SELECT CASE WHEN CC.Age >= 60
+				AND COUNT(CV_EC.Condition) > 0 then 1
+				WHEN CC.Age >= 60
+				OR COUNT(CV_EC.Condition) > 0 then 2 end
+			FROM COVID19Screening CV_S (nolock)
+			LEFT JOIN custom_COVID_in_existing_conditions CV_EC (nolock) 
+				ON CV_S.COVIDID=CV_EC.COVIDID 
+				AND CV_S.ActiveStatus <> 'D' 
+			LEFT JOIN ClientCalculations CC
+				ON CC.ClientID = CV_S.ClientID
+			WHERE C.ClientID = CV_S.ClientID
+			GROUP BY CV_S.ClientID, CC.Age) AS [COVIDScore]
+	FROM Custom_VW_PrioritizationList PL (nolock)
+	INNER JOIN cmClient C (nolock) 
+		ON PL.ClientID = C.ClientID) AS T
+WHERE Custom_VW_PrioritizationList.CaseID = T.CaseID
+GROUP BY T.CaseID)
 
+---------------------------------------------------
+-------- use in filters to join client to ---------
+------------ most recent enrollment ---------------
+---------------------------------------------------
 
-
-
-
-
-
+(SELECT max(E.[EnrollID])
+FROM Enrollment E (NOLOCK) 
+INNER JOIN EnrollmentCase EC(NOLOCK) 
+	ON E.CaseID=EC.CaseID
+		AND EC.ActiveStatus <> 'D'
+INNER JOIN 
+	(SELECT E.ClientID, max(E.[EnrollDate]) AS max_EnrollDate
+		FROM Enrollment E (NOLOCK) 
+		INNER JOIN EnrollmentCase EC(NOLOCK) 
+			ON E.CaseID=EC.CaseID
+				AND EC.ActiveStatus <> 'D'
+		WHERE E.ClientID = HMIS_LivingSituation.[ClientID]
+		GROUP BY E.[ClientID]) AS E_D
+	ON E_D.max_EnrollDate = E.EnrollDate
+		AND E_D.ClientID = E.ClientID
+WHERE E.ClientID = HMIS_LivingSituation.[ClientID]
+GROUP BY E.[ClientID]		 
+)
 
